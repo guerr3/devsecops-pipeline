d4277965570cc0117201769f5f461892e7a80b7a
Warre Gehre <warregehre06@gmail.cm>
2024-12-19 16:29:25 +0100

    Adjusted module versions to pass security check

--- requirements.py
+++ requirements.py
@@ -1,5 +1,5 @@
 # requirements.txt
 
-flask==2.0.0  # Oudere versie met bekende kwetsbaarheden
-requests==2.25.0  # Oudere versie met bekende kwetsbaarheden
+flask==3.1.0  # Oudere versie met bekende kwetsbaarheden
+requests==2.7.0 # Oudere versie met bekende kwetsbaarheden
 python-dotenv==0.19.0
